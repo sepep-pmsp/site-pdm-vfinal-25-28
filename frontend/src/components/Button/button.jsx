9a4756eda5e808d1fa2a61316c4ef505c76bbbc9
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-07-21 20:01:31 -0300

    config files (#1)
    adding reusable components and configuration files

---
+++ frontend/src/components/Button/button.jsx
@@ -0,0 +1,44 @@
+import "../../style/_button.css";
+import { scrollToSection } from "../../utils/scroll";
+
+export default function CustomButton({
+  type,
+  label,
+  target,
+  onClick,
+  className,
+  style
+}) {
+  const handleClick = () => {
+    if (type === "scroll") {
+      scrollToSection(target);
+    } else if (type === "download") {
+      const link = document.createElement("a");
+      link.href = target;
+      link.download = target.split("/").pop();
+      link.click();
+    } else if (type === "modal") {
+      onClick && onClick();
+    } else if (type === "link") {
+      window.location.href = target;
+    }
+  };
+
+  const buttonClass = `custom-button ${
+    type === "scroll"
+      ? "btn-scroll"
+      : type === "download"
+      ? "btn-download"
+      : type === "modal"
+      ? "btn-modal"
+      : type === "link"
+      ? "btn-link"
+      : ""
+  } ${className || ""}`;
+
+  return (
+    <button onClick={handleClick} className={buttonClass} style={style}>
+      {label}
+    </button>
+  );
+}
