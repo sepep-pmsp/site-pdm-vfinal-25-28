1bd816336b96864ad13f43d8ccc0fc7b1780755a
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-07-23 20:18:30 -0300

    Create About.jsx

---
+++ frontend/src/page/home/sections/section-about/About.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import Matarazzo from "@/assets/svg/foto_matarazzo.svg";
+import CustomButton from "@//components/Button/Button";
+import { getAboutData } from "@/services/home/getAboutData";
+
+export default function About() {
+  const [about, setAbout] = useState(null);
+
+  useEffect(() => {
+    getAboutData().then(setAbout).catch(console.error);
+  }, []);
+
+  if (!about) return <div>Carregando...</div>;
+  return (
+    <div className="mx-24">
+      <section>
+        <div>
+          <div className="my-8">
+            <h2 className="text-8xl text-[var(--color-navy)]">
+              {about.titulo}
+            </h2>
+            <div className="linha"></div>
+          </div>
+          <div className="my-4 flex items-center flex-nowrap relative top-8">
+            <div className="flex justify-end items-center">
+              <img className="w-[85%]" src={Matarazzo} alt="" />
+            </div>
+            <div className="bg-[color:var(--color-navy)] h-[35rem] w-[45rem] py-4 px-10 rounded-tr-3xl rounded-br-3xl flex items-start flex-col justify-center gap-8">
+              <h3 className="text-white text-6xl">{about.subtitulo}</h3>
+              <p className="text-white text-3xl">{about.paragrafo}</p>
+            </div>
+          </div>
+          <div className="w-48 relative h-20 left-[70rem] bottom-[3.5rem]">
+            <CustomButton
+              type="link"
+              target="download"
+              className="all_buttons uppercase"
+            >
+              <p className="btn-about">saiba +</p>
+            </CustomButton>
+          </div>
+        </div>
+        <div className="linha"></div>
+      </section>
+    </div>
+  );
+}
