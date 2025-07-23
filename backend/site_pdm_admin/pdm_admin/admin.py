4f9c52d2bb89077409b08aa2ebcd8e7355591d26
hpgy <hpougy@gmail.com>
2025-07-23 16:59:15 -0300

    divisoes administrativas

--- backend/site_pdm_admin/pdm_admin/admin.py
+++ backend/site_pdm_admin/pdm_admin/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models.regionalizacao.divisoes_administrativas import SubPrefeitura, Distrito
 # Register your models here.
+
+admin.site.register(SubPrefeitura)
+admin.site.register(Distrito)
