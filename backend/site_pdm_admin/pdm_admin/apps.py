ae9a75928b276caeec5134047b1864b662034810
hpgy <hpougy@gmail.com>
2025-07-23 17:36:22 -0300

    label para o modulo no django admin

--- backend/site_pdm_admin/pdm_admin/apps.py
+++ backend/site_pdm_admin/pdm_admin/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PdmAdminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pdm_admin'
+    verbose_name = "Cadastros Básicos"
