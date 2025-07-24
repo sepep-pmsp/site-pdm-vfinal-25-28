9102e32b9b7617febe1a1b59be810a4a7ba3a775
Henrique Pougy <41967884+h-pgy@users.noreply.github.com>
2025-07-24 15:28:25 -0300

    Cadastros basicos (#3)
    * divisoes administrativas
    
    * adicionando bib de configuracao do django admin
    
    * exemplo de config
    
    * configurando para pt br
    
    * label para o modulo no django admin
    
    * tipos de orgao
    
    * adding django jazzmin to reqs
    
    * hotfix bug frontend
    
    * simplificando orgaos

--- backend/site_pdm_admin/pdm_admin/admin.py
+++ backend/site_pdm_admin/pdm_admin/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models.regionalizacao.divisoes_administrativas import SubPrefeitura, Distrito
+from .models.estrutura_administrativa.orgaos import Orgao
 # Register your models here.
+
+admin.site.register(SubPrefeitura)
+admin.site.register(Distrito)
+admin.site.register(Orgao)
