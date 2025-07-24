cec38ed398b10d3ecc29920080a3b01874fced22
h-pgy <hpougy@gmail.com>
2025-07-24 15:23:53 -0300

    simplificando orgaos

--- backend/site_pdm_admin/pdm_admin/admin.py
+++ backend/site_pdm_admin/pdm_admin/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from .models.regionalizacao.divisoes_administrativas import SubPrefeitura, Distrito
-from .models.estrutura_administrativa.entes import CategoriaEnte, Ente
 from .models.estrutura_administrativa.orgaos import Orgao
 # Register your models here.
 
 admin.site.register(SubPrefeitura)
 admin.site.register(Distrito)
-admin.site.register(CategoriaEnte)
-admin.site.register(Ente)
 admin.site.register(Orgao)
--- backend/site_pdm_admin/pdm_admin/models/estrutura_administrativa/orgaos.py
+++ backend/site_pdm_admin/pdm_admin/models/estrutura_administrativa/orgaos.py
@@ -1,11 +1,20 @@
 from django.db import models
 
-from .entes import Ente
+class TipoOrgao(models.TextChoices):
+
+    INDIRETA = 'INDIRETA', 'Administração Indireta'
+    DIRETA = 'DIRETA', 'Administração Direta'
+    CAMARA = 'CAMARA', 'Câmara Municipal'
+    TCM = 'TCM', 'Tribunal de Contas do Município'
 
 class Orgao(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome do Órgão')
     sigla = models.CharField(max_length=10, verbose_name='Sigla do Órgão')
-    ente = models.ForeignKey(Ente, on_delete=models.CASCADE, related_name='orgaos', verbose_name='Ente ao qual pertence o Órgão')
+    tipo =models.CharField(
+        max_length=20,
+        choices=TipoOrgao.choices,
+        default=TipoOrgao.DIRETA,
+    )
 
     class Meta:
         verbose_name = 'Órgão'
