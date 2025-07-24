3f5dca01d817126dafe91e5b89b2760b90c8f662
h-pgy <hpougy@gmail.com>
2025-07-24 17:34:35 -0300

    eixos e temas v1

--- backend/site_pdm_admin/estrutura_pdm/admin.py
+++ backend/site_pdm_admin/estrutura_pdm/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models.eixos.eixos_estrategicos import Eixo
+from .models.eixos.temas import Tema
+
 # Register your models here.
+admin.site.register(Tema)
+
+class EixoInline(admin.TabularInline):
+    model = Tema
+    extra = 1
+    verbose_name = "Tema"
+    verbose_name_plural = "Temas"
+
+@admin.register(Eixo)
+class EixoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao', 'resumo')
+    search_fields = ('nome', 'descricao')
+    inlines = [EixoInline]
---
+++ backend/site_pdm_admin/estrutura_pdm/models/eixos/temas.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .eixos_estrategicos import Eixo
+
+class Tema(models.Model):
+    nome = models.CharField(max_length=100, unique=True, verbose_name="Nome do Tema")
+    descricao = models.TextField(blank=True, null=True, verbose_name="Descrição do Tema")
+    eixos = models.ForeignKey(Eixo, blank=False, related_name='temas', 
+                                        verbose_name="Eixo relacionado", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Tema"
+        verbose_name_plural = "Temas"
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
