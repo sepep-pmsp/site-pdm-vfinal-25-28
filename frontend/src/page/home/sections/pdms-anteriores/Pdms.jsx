9a81a74f48a95cd8890897acab3454647152b113
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-07-31 11:26:20 -0300

    Update Pdms.jsx

--- frontend/src/page/home/sections/pdms-anteriores/Pdms.jsx
+++ frontend/src/page/home/sections/pdms-anteriores/Pdms.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import CustomButton from "@/components/Button/Button";
 
 export default function Pdms() {
+  const navigate = useNavigate();
+  const goTo = (path) => {
+    navigate(path);
+  };
   return (
     <div className="py-12">
       <section>
         <div className="flex items-center justify-center flex-row flex-nowrap gap-28">
           <div className="flex flex-row justify-center gap-4">
-            <p className="text-2xl uppercase text-[var(--color-navy)]">e mais: </p>
+            <p className="text-2xl uppercase text-[var(--color-navy)]">
+              e mais:{" "}
+            </p>
             <h2 className="text-4xl w-[28rem] text-[var(--color-navy)]">
               Conheça todos os outros Programas de Metas já criados para São
               Paulo!
@@ -15,8 +22,9 @@ export default function Pdms() {
           </div>
           <div>
             <CustomButton
+              onClick={() => goTo("/historico")}
               type="link"
-              target="download"
+              target="#/historico"
               className="all_buttons uppercase"
             >
               <p className="btn-about p-2">
