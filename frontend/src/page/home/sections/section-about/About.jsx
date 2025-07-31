b74c680c0d0c4280f7d04cb20fe9b00c3086878b
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-07-31 10:04:58 -0300

    Card palavraprefeito (#10)
    * adicionando animações de entrada e saida
    
    * adicionado carta do prefeito
    
    * adicionado itens da carta do prefeito
    
    * Criado Modal para a carta do prefeito
    
    efeito de abertura e saída do modal
    bloqueado scroll da pagina quando modal estiver aberto
    puxando informações do getAboutData

--- frontend/src/page/home/sections/section-about/About.jsx
+++ frontend/src/page/home/sections/section-about/About.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Matarazzo from "@/assets/svg/foto_matarazzo.svg";
 import CustomButton from "@/components/Button/Button";
+import ModalPrefeito from "@/components/ModalPrefeito/ModalPrefeito";
 import { getAboutData } from "@/services/home/getAboutData";
 
 export default function About() {
   const [about, setAbout] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     getAboutData().then(setAbout).catch(console.error);
   }, []);
 
   if (!about) return <div>Carregando...</div>;
+
   return (
-    <div className="my-34">
+    <div className="my-34 relative">
       <section>
         <div>
           <div className="my-8">
@@ -21,6 +24,7 @@ export default function About() {
             </h2>
             <div className="linha"></div>
           </div>
+
           <div className="my-4 flex items-center flex-nowrap relative top-8">
             <div className="flex justify-end items-center">
               <img className="w-[85%]" src={Matarazzo} alt="" />
@@ -30,6 +34,8 @@ export default function About() {
               <p className="text-white text-3xl">{about.paragrafo}</p>
             </div>
           </div>
+
+          {/* Botão Saiba+ */}
           <div className="w-48 relative h-20 left-[75rem] bottom-[2.5rem]">
             <CustomButton
               type="link"
@@ -39,18 +45,27 @@ export default function About() {
               <p className="btn-about">saiba +</p>
             </CustomButton>
           </div>
+
+          {/* Botão que abre modal */}
           <div className="w-48 relative h-20 left-[57rem] bottom-[7.5rem]">
-                <CustomButton
-                type="link"
-                target="download"
-                className="all_buttons uppercase"
-                >
-                    <p>palavra do prefeito <br></br> <strong>leia aqui!</strong></p> 
-                </CustomButton>
-              </div>
+            <CustomButton
+              type="modal"
+              onClick={() => setShowModal(true)}
+              className="all_buttons uppercase"
+            >
+              <p>
+                palavra do prefeito <br /> <strong>leia aqui!</strong>
+              </p>
+            </CustomButton>
+          </div>
         </div>
         <div className="linha"></div>
       </section>
+      <ModalPrefeito
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+        carta={about.carta_prefeito}
+      />
     </div>
   );
 }
