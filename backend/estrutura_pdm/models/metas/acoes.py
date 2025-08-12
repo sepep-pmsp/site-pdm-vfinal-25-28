e7e1856743ef6d775e8e201f3030be8ac09cee69
hpgy <hpougy@gmail.com>
2025-08-12 17:53:43 -0300

    seed acoes estrategicas - mas precisa melhorar porque pode ter mais de uma secretaria responsavel

--- backend/estrutura_pdm/models/metas/acoes.py
+++ backend/estrutura_pdm/models/metas/acoes.py
@@ -1,16 +1,28 @@
 from django.db import models
 from .metas import Meta
+from cadastros_basicos.models.estrutura_administrativa import Orgao
 
 class AcaoEstrategica(models.Model):
 
     posicao = models.IntegerField(blank=False, null=False, verbose_name="Ordem de Apresentação da Ação Estratégica")
     descricao = models.CharField(max_length=100, unique=True, verbose_name="Descrição da Ação Estratégica")
-    
+    numero = models.CharField(max_length=20, unique=True, verbose_name="Número da Ação Estratégica")
+
     meta = models.ForeignKey(
         Meta,
         blank=False,
+        null=False,
         related_name='acoes_estrategicas',
         verbose_name="Meta relacionada",
+        on_delete=models.CASCADE
+    )
+
+    orgao_responsavel = models.ForeignKey(
+        Orgao,
+        blank=False,
+        null=False,
+        related_name='acoes_estrategicas',
+        verbose_name="Órgão Responsável",
         on_delete=models.CASCADE
     )
 
