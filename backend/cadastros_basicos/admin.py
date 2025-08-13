2ed222e5e6b3e3e89b9c4528f5e2a7d8708a9364
Henrique Pougy <41967884+h-pgy@users.noreply.github.com>
2025-08-13 12:20:31 -0300

    Ods com seed (#19)
    * refactor caminho imagens
    
    * model ods
    
    * dados para ods
    
    * seed para ods

--- backend/cadastros_basicos/admin.py
+++ backend/cadastros_basicos/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models.regionalizacao import SubPrefeitura, Distrito
 from .models.estrutura_administrativa import Orgao, Prefeito
+from .models.vinculos_externos.ods import ODS
 # Register your models here.
 
 admin.site.register(SubPrefeitura)
 admin.site.register(Distrito)
 admin.site.register(Orgao)
 admin.site.register(Prefeito)
+admin.site.register(ODS)
---
+++ backend/cadastros_basicos/models/vinculos_externos/ods.py
@@ -0,0 +1,52 @@
+from django.db import models
+from static_files.models import Imagem
+from django.core.validators import RegexValidator
+
+hex_color_validator = RegexValidator(
+    regex=r'^#([A-Fa-f0-9]{6})$',
+    message='Informe uma cor hexadecimal no formato #RRGGBB (ex: #1A2B3C).'
+)
+
+class ODS(models.Model):
+
+    numero = models.PositiveIntegerField(
+        unique=True,
+        verbose_name="Número da ODS",
+        help_text="Número da ODS (ex: 1, 2, 3, etc.)",
+        
+    )
+    nome = models.CharField(max_length=100, unique=True, verbose_name="Nome do ODS")
+    descricao = models.TextField(blank=True, null=True, verbose_name="Descrição do ODS")
+    
+    logo_colorido = models.ForeignKey(
+        Imagem,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="Logo do ODS",
+        related_name="ods"
+    )
+
+    logo_branco = models.ForeignKey(
+        Imagem,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="Logo do ODS (Branco)",
+        related_name="ods_branco"
+    )
+
+    cor_principal = models.CharField(
+        max_length=7,
+        default="#000000",
+        validators=[hex_color_validator],
+        verbose_name="Cor Principal do ODS"
+    )
+    
+    class Meta:
+        verbose_name = "ODS"
+        verbose_name_plural = "ODS"
+        ordering = ['numero']
+
+    def __str__(self):
+        return self.nome
