e85ce94fbcfcbaebc0f7aba23cb7fd6731256755
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-08-22 09:37:24 -0300

    Update SectionIndicadores.jsx

--- frontend/src/page/Sobre/sections-sobre/section-indicadores/SectionIndicadores.jsx
+++ frontend/src/page/Sobre/sections-sobre/section-indicadores/SectionIndicadores.jsx
@@ -6,11 +6,11 @@ import bgDetalhes2 from "@/assets/svg/img_fundo2.svg";
 
 export default function SectionIndicadores({ sobre }) {
   return (
-    <div className="my-16">
+    <div className="my-16 mx-24">
       <div className="bg-[var(--color-navy)] h-1 w-[86rem] mb-8"></div>
-      <img className='absolute w-[38rem] left-[81.09rem] top-[154rem]' src={bgCell} alt="" />
-      <img className='absolute z-[-1] top-[191rem] left-4 w-56' src={bgDetalhes1} alt="" />
-      <img className='absolute z-[-1] top-[207rem] left-4 w-56' src={bgDetalhes2} alt="" />
+      <img className='absolute w-[38rem] left-[81.09rem] top-[159rem]' src={bgCell} alt="" />
+      <img className='absolute z-[-1] top-[192rem] left-4 w-56' src={bgDetalhes1} alt="" />
+      <img className='absolute z-[-1] top-[209rem] left-4 w-56' src={bgDetalhes2} alt="" />
       <div className="flex flex-col items-start justify-center gap-40 w-[60rem] relative left-24">
         {sobre.indicadores.map((item, index) => (
           <div key={index} className="mb-10">
