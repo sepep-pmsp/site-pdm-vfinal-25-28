58fe755940ea894050d4a40a88080a70277d94e0
h-pgy <hpougy@gmail.com>
2025-08-25 15:49:36 -0300

    getting published historico section

---
+++ backend/secoes_pagina_inicial/queries/historico.py
@@ -0,0 +1,13 @@
+from secoes_pagina_inicial.models import Historico
+
+def get_published_historico() -> Historico | None:
+
+    historicos = Historico.objects.filter(publicado=True)
+
+    historicos = list(historicos)
+    if len(historicos) > 1:
+        raise RuntimeError("Existe mais de um histórico publicado.")
+    elif len(historicos) == 1:
+        return historicos[0]
+    else:
+        return None
