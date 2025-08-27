0db11a990eddf9067abecdae02f9de98e233c56d
h-pgy <hpougy@gmail.com>
2025-08-27 17:59:07 -0300

    fix quebrou link imagem depois dos namespaces

--- backend/pdm_api/utils/static_files/images.py
+++ backend/pdm_api/utils/static_files/images.py
@@ -9,10 +9,11 @@ def get_rel_link(img:Imagem) -> str:
     """
     Retorna o caminho relativo da imagem.
     """
-    return f'images/{img.id}' if img else ''
+    return f'api/static/images/{img.id}' if img else ''
 
-def get_abs_link(request, img:Imagem) -> str:
-    """
-    Retorna o caminho absoluto da imagem.
-    """
-    return request.build_absolute_uri(get_rel_link(img)) if img else ''
+def get_abs_link(request, img: Imagem) -> str:
+    if not img:
+        return ""
+    rel = get_rel_link(img)                       # ex: "api/static/image/1"
+    abs_path = "/" + rel.lstrip("/")              # garante caminho absoluto
+    return request.build_absolute_uri(abs_path)
