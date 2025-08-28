a96cfcd43b9d189a70ecf0ac369ecb7e8ab39857
Henrique Pougy <41967884+h-pgy@users.noreply.github.com>
2025-08-27 23:13:31 -0300

    Search metas (#35)
    * adding relacionamento direto entre meta e zona
    
    * vinculo entre metas e ods e planos setoriais
    
    * atributos expondo siglas dos relacionamentos em lista
    
    * schema para a busca
    
    * bnuscas no banco de dados
    
    * endpoint

--- backend/estrutura_pdm/models/metas/__init__.py
+++ backend/estrutura_pdm/models/metas/__init__.py
@@ -1,2 +1,2 @@
-from .metas import Meta, MetaOrgao, MetaSubprefeitura
+from .metas import Meta, MetaOrgao, MetaSubprefeitura, MetaZona, MetaODS, MetaPlanoSetorial
 from .acoes import AcaoEstrategica, AcaoOrgao
