e82a0312b0ed88c3aeaca6bb85ac6cf636dc8541
Ana Carolina Prefeitura <121954394+Ana-CAS-Machado@users.noreply.github.com>
2025-08-29 21:09:48 -0300

    Create getParticipacaoData.js

---
+++ frontend/src/services/Participacao/getParticipacaoData.js
@@ -0,0 +1,14 @@
+import { API_BASE_URL, USE_API } from "../config";
+
+export async function getParticipacaoData() {
+  if (USE_API) {
+    const response = await fetch(`${API_BASE_URL}/FiltroMetas`);
+    if (!response.ok) {
+      throw new Error("Erro ao carregar dados do Participação social ");
+    }
+    return await response.json();
+  } else {
+    const data = await import("@/mock/json/participacaoSocial.json");
+    return data.default;
+  }
+}
