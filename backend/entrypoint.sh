e0553e758e126692c4941f0958abb7edf4b48ec4
hpgy <hpougy@gmail.com>
2025-09-01 16:24:19 -0300

    fixes no seed e adicionando no entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -31,6 +31,7 @@ python manage.py seed_transparencia
 python manage.py seed_regionalizacao
 python manage.py seed_devolutivas
 python manage.py seed_secao_devolutivas
+python manage.py seed_sobre
 
 echo ">> Iniciando servidor"
 exec gunicorn site_pdm_admin.wsgi:application --bind 0.0.0.0:8000
